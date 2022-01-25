ed6d54fd07fb960380a621e31f9185b5bfa2c856
Valentine Education Center <val.edu.center@gmail.com>
2022-01-25 13:43:38 -0600

    initiale parse import

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,14 @@ import 'react-toastify/dist/ReactToastify.css'
 import AccountsPage from './accounts/AccountsPage'
 import UnauthorizedPage from './common/UnauthorizedPage'
 import LogoutPage from './logout/LogoutPage'
+import Parse from 'parse/dist/parse.min.js';
+
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = 'pen8j01Zc3JaqVkHmMCbZud1AhOaOMeBYTpzlUhw';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'RJQoYRy9K5rQCB9J5YxEnp1ygWvXefCJIqJkJNn3';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
     return (
