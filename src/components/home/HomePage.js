bddb73c8c93783f7e219ee6a6a2f2241e6fe4b42
Valentine Education Center <val.edu.center@gmail.com>
2022-04-22 15:19:06 -0500

    isaiah home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -8,6 +8,7 @@ const HomePage = () => (
             <img style={{height: "400px"}} className="home-page-img" src="https://pbs.twimg.com/media/FKhqe2GXMAMpUKT?format=png"/>
             <img className="home-page-img" src="https://bgcc.org/wp-content/uploads/2021/08/DSC_0564-445x340.jpg"/>
             <img className="home-page-img" src="https://bgcc.org/wp-content/uploads/2021/08/Group-with-Ron-Kittle-453x340.jpg"/>
+            <img className="home-page-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST2oOD4CUHhEpTXnW6pmwoelA23MkqDvdcww&usqp=CAU"/>
         </div>
         <Link to="about" className="btn btn-primary btn-lg">
             Learn More
